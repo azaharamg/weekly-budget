3f2f2ae98484f06bda0ec801ae7ac6192f1ea985
Azahara <azaharamaga@gmail.com>
2020-03-12 12:30:19 +0100

    Add budget and validate it

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,43 @@
 import React, { useState } from 'react';
+import Error from './Error';
 
 const Form = () => {
+  const [name, storeName] = useState('');
+  const [amount, storeAmount] = useState(0);
+  const [error, storeError] = useState(false);
+
+  const addBudget = e => {
+    e.preventDefault();
+    if (amount < 1 || isNaN(amount) || name.trim() === '') {
+      storeError(true);
+      return;
+    }
+    storeError(false);
+  };
+
   return (
-    <form>
+    <form onSubmit={addBudget}>
       <h2>Agrega tus gatos aquí</h2>
+      {error ? <Error message='Ambos campos son obligatorios o Presupuesto Incorrecto' /> : null}
       <div className='campo'>
         <label>Nombre Gasto</label>
-        <input type='text' className='u-full-width' placeholder='Ej. Transporte' />
+        <input
+          type='text'
+          className='u-full-width'
+          placeholder='Ej. Transporte'
+          value={name}
+          onChange={e => storeName(e.target.value)}
+        />
       </div>
       <div className='campo'>
         <label>Cantidad Gasto</label>
-        <input type='number' className='u-full-width' placeholder='Ej. 300' />
+        <input
+          type='number'
+          className='u-full-width'
+          placeholder='Ej. 300'
+          value={amount}
+          onChange={e => storeAmount(parseInt(e.target.value))}
+        />
       </div>
       <input type='submit' className='button-primary u-full-width' value='Agregar gasto' />
     </form>
